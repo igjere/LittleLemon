5c9c1a8f3eee022844b83ea21f86dc692b6a7007
jerry <ijeremiahgarcia@gmail.com>
2024-12-10 02:37:22 -0600

    static routes

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html', {
+        'current_year': datetime.now().year
+    })
